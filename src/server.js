5be68213d5bc78475c24cb452f833769a060c623
Rafael <rafaelw955@outlook.com>
2026-06-24 10:44:53 -0300

    implement: profile get and update

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import cartRouter from './routes/cartRouter.js';
 import AIRouter from './routes/AIRouter.js';
 import errorHandler from './middleware/errorHandler.js';
 import registerRouter from './routes/registerRouter.js'
+import profileRouter from './routes/profileRouter.js'
 import { authMiddleware } from './middleware/auth.js';
 
 const app = express();
@@ -16,8 +17,9 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users/register", registerRouter)
 app.use(authMiddleware);
-app.use("/api/cart", cartRouter);
 app.use("/api/users", userRouter);
+app.use('/api/profile', profileRouter)
+app.use("/api/cart", cartRouter);
 app.use('/api/ai', AIRouter)
 app.use(errorHandler);
 
