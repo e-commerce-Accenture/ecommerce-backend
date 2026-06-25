8e6f273ce959609b4cb9a994e1ec90c4019c51b7
Rafael <rafaelw955@outlook.com>
2026-06-25 20:20:04 -0300

    implement: change password user in profile

--- functions/src/routes/profileRouter.js
+++ functions/src/routes/profileRouter.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { ProfileController } from "../controllers/profileController.js";
 import { validateRequest } from "../middleware/vallidation.js";
-import { updateProfileSchema } from "../controllers/schemas/profileSchema.js";
+import { updatePassowrdSchema, updateProfileSchema } from "../controllers/schemas/profileSchema.js";
 
 const profileController = new ProfileController();
 const router = Router();
 
 router
     .get('/', profileController.getProfile)
-    .patch('/', validateRequest(updateProfileSchema), profileController.updateProfile);
+    .patch('/', validateRequest(updateProfileSchema), profileController.updateProfile)
+    .patch('/change-password', validateRequest(updatePassowrdSchema), profileController.updatePassword);
 
 export default router;
